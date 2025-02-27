1f0e05f9de0a9d32e55fb996021046664c742543
dyno-star <da893@drexel.edu>
2025-02-27 17:16:49 -0500

    Design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
 
   return (
     <div className="App">
+      <h1>
+        Itenary for 7 Days in Chicago🛫 
+      </h1>
+
+      <h2>
+        Welcome to Chicago, Check out this 
+        calendar to get to know and see all the sights during your stay.
+      </h2>
+      
+      <Calendar />
 
       
     </div>
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,8 +3,113 @@ import React from "react";
 const Calender = () => {
     return (
         <div className="Calender">
-            Testing the calender component
-
+            <table>
+  <thead>
+    <tr>
+      <th></th>
+      <th>Sunday</th>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Thursday</th>
+      <th>Friday</th>
+      <th>Saturday</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+  <td className="time">8 am</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">9 am</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">10 am</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">11 am</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">12pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">1 pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">2 pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">3 pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">4 pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr><tr>
+  <td className="time">5 pm</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+  </tbody>  
+</table>
         </div>
     )
 }
