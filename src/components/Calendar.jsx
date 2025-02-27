11efa10982c5720ffa81a9464ab79143a66c8aed
dyno-star <da893@drexel.edu>
2025-02-27 16:57:44 -0500

    added calendar

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Calender = () => {
+    return (
+        <div className="Calender">
+            Testing the calender component
+
+        </div>
+    )
+}
+
+export default Calender;
