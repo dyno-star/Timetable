9ac7602afcd55de9e1c1f319cdf2d7c8ffcb8e46
dyno-star <da893@drexel.edu>
2025-02-28 08:16:27 -0500

    added location

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Event = (props) => {
-    return (
-        <td className={'Event ' + props.color}>
-           <h5>{props.event}</h5>
-
-        </td>
-    )
-}
+const Event = ({ event, location, color }) => {
+  return (
+    <div
+      className="p-2 rounded-lg text-white text-sm font-semibold shadow-md"
+      style={{ backgroundColor: color }}
+    >
+      <h5>{event}</h5>
+      {location && <p className="text-xs font-normal opacity-80">{location}</p>}
+    </div>
+  );
+};
 
 export default Event;
